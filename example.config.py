1b29f4d951a636417db5b81d9887cfdbb4fb8ac1
rugerclaus <rogerrock12games@gmail.com>
2026-04-15 06:21:36 -0600

    updated example config

--- example.config.py
+++ example.config.py
@@ -1,9 +1,9 @@
 import platform
 
 config = {
-    "TITLE": "Snow Blitz Beta Play Test Demo",
+    "TITLE": "Distant Realms Alpha",
     "AUTHORS": ["Roger Falck", "Reina Meza"],
-    "VERSION": "0.9.8-beta",
+    "VERSION": "0.0.2",
     "OS": platform.system(),
     "UPDATE_ZIP_NAME": "snowblitz_update_latest.zip",
     "UPDATER_WINDOWS": "updater.exe",
